8c6ece13a5df21735ec19dfcd1981061b29e31d8
yxyki <y1432621251@qq.com>
2022-03-31 07:56:33 +0000

    新增

--- 01_dart_start.dart
+++ 01_dart_start.dart
@@ -3,5 +3,15 @@ void variables(){
   var name='Voyager';
   var year=1977;
   var antennaDiameter=3.7;
+  var flybyObjects=['Jupiter','Saturn','Uranus','Neptune'];
+  var image={
+    'tags':['saturn'],
+    'url':'//path/to/saturn.jpg'
+  };
+  print('name is $name,year is $year,antennaDiameter is $antennaDiameter,flybyObjects is $flybyObjects,image is$image');
+}
 
+void main(List<String> args){
+//变量
+    variables();
 }
